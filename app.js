747d2feeeb4238b417e61a4e75362da998255b16
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2020-11-07 22:04:45 +0200

    fixed google  api url

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://cslectures.herokuapp.com//auth/google/cslectures",
+    callbackURL: "https://cslectures.herokuapp.com/auth/google/cslectures",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
