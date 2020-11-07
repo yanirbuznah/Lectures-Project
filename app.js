21c5cfad61baaca7eeb6feec832145a6d0e2beee
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2020-11-07 21:57:53 +0200

    fixed req problem for login from google and facebook

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/cslectures",
+    callbackURL: "https://cslectures.herokuapp.com//auth/google/cslectures",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -78,7 +78,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/csLectures",
+    callbackURL: "https://cslectures.herokuapp.com/auth/facebook/csLectures",
     profileFields: ['id', 'displayName', 'photos', 'emails']
   },
   function(accessToken, refreshToken, profile, cb) {
