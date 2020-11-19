0a922b8b0e4825764978d909bf0b91884aff3232
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2020-11-19 23:38:43 +0200

    add slides of lecture 6

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -88,4 +88,28 @@
   //     player.seekTo(seconds, true);
   //   }
   // }
+
+
+  $('#recipeCarousel').carousel({
+    interval: 10000
+  })
+
+  $('.carousel .carousel-item').each(function() {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if(!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for(var i = 0; i < minPerSlide; i++) {
+      next = next.next();
+      if(!next.length) {
+        next = $(this).siblings(':first');
+      }
+
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
+
 })(jQuery); // End of use strict
